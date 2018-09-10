572e515617d8df658e54aa8723cad3b2598cfafd
cd <coraldarling@outlook.com>
2018-09-10 20:26:38 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -124,5 +124,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  programmer_hash[:dennis_ritchie][:languages][].push("Assembly")
+  programmer_hash[:dennis_ritchie][:languages][1].push("Assembly")
 end
