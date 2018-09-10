70ae798f0581dcf06ff7ecd2e58e08ebe33fb771
cd <coraldarling@outlook.com>
2018-09-10 20:52:49 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -105,7 +105,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
   programmer_hash[:alan_kay][:known_for] = "GUI"   
-     binding.pry
+  
+  programmer_hash
 end
 
 def adding_to_dennis
@@ -125,5 +126,6 @@ def adding_to_dennis
         }
      }
   programmer_hash[:dennis_ritchie][:languages] = ["C"] << "Assembly"
-  
+
+  programmer_hash
 end
